518417f8560b4a50dd8702d749daff6093a813cf
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-07-03 06:37:18 +0700

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -22,10 +22,17 @@ logging.basicConfig(level=logging.INFO)
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['start']))
 def _start(client, message):
-    client.send_message(message.chat.id,
-        sticker=("CAACAgQAAx0CTv65QgABBfJlYF6VCrGMm6OJ23AxHmD6qUSWESsAAhoQAAKm8XEeD5nrjz5IJFYeBA"),
-        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
-        parse_mode="markdown",
+    await message.reply_sticker("CAACAgQAAx0CTv65QgABBfJlYF6VCrGMm6OJ23AxHmD6qUSWESsAAhoQAAKm8XEeD5nrjz5IJFYeBA")
+    await message.reply_text(
+        f"""<b>**Hello Friends**, {message.from_user.first_name}! 👋
+\nSaya 𝐂𝐚𝐧𝐝𝐮𝐌𝐮𝐬𝐢𝐜𝐁𝐨𝐭, Saya akan membantumu **memutar music** di Voice Chat Telegram Groups & Channel, dengan **fitur-fitur** yang menarik.
+\n─────────────────────────────────
+𝑺𝒆𝒎𝒖𝒂 𝒐𝒓𝒂𝒏𝒈 𝒑𝒂𝒔𝒕𝒊 𝒎𝒂𝒕𝒊, 𝒕𝒂𝒑𝒊 𝒕𝒊𝒅𝒂𝒌 𝒔𝒆𝒎𝒖𝒂 𝒐𝒓𝒂𝒏𝒈 𝒅𝒂𝒑𝒂𝒕 𝒎𝒆𝒎𝒃𝒆𝒓𝒊 𝒂𝒓𝒕𝒊. 𝑷𝒂𝒔𝒕𝒊𝒌𝒂𝒏 𝒉𝒊𝒅𝒖𝒑𝒎𝒖 𝒃𝒆𝒓𝒂𝒓𝒕𝒊/𝒃𝒆𝒓𝒎𝒂𝒏𝒇𝒂𝒂𝒕 𝒖𝒏𝒕𝒖𝒌 𝒐𝒓𝒂𝒏𝒈 𝒍𝒂𝒊𝒏. 
+─────────────────────────────────
+\n❃ Manage by : [°ᴹᴿ° | ℝ𝕆𝔹𝕆𝕋](https://t.me/justthetech) 
+❃ Support dengan doa aja guys! Thanks!
+❃ NB : Maaf jika ada kekurangan didalam bot ini.
+        </b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -45,8 +52,8 @@ def _start(client, message):
                 ]
             ]
         ),
-        reply_to_message_id=message.message_id
-        )
+     disable_web_page_preview=True
+    )
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['help']))
 def _help(client, message):
