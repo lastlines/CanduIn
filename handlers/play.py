b0736a964530197690e8ef327b82921c4bd2a168
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-07-02 19:51:02 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -18,7 +18,7 @@ import converter
 from downloaders import youtube
 from config import DURATION_LIMIT
 from helpers.filters import command, other_filters
-from helpers.decorators import errors, , authorized_users_only
+from helpers.decorators import errors, authorized_users_only
 from helpers.errors import DurationLimitError
 from helpers.gets import get_url, get_file_name
 import aiofiles
